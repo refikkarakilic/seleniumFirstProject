8b08114a119fa8ce5be0364bf39d11ac3f5b41fa
refik <rfkkrklc95@gmail.com>
2024-04-17 00:19:57 +0300

    step1 amazon web sayfasına git step2 çerezleri reddet step3 search kutusuna "laptop" yazıp arat step4 karşınıza gelen ilk ürün resmine tıkla step5 2 saniye bekletip driverı kapatın

---
+++ src/test/java/Checkbox.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Checkbox {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver","C:\\project\\chromedriver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        //https://the-internet.herokuapp.com/checkboxes
+        String url = "https://the-internet.herokuapp.com/checkboxes";
+        driver.get(url);
+
+        //b. Checkbox1 ve checkbox2 elementlerini locate edin.
+        WebElement checkBox1 = driver.findElement(By.xpath("(//input[@type='checkbox'])[1]"));
+        WebElement checkBox2 = driver.findElement(By.xpath("(//input[@type='checkbox'])[2]"));
+
+
+        //c. Checkbox1 seçili değilse onay kutusunu tıklayın
+        if(!checkBox1.isSelected()){
+            Thread.sleep(2000);
+            checkBox1.click();
+        }
+
+        //d. Checkbox2 seçili değilse onay kutusunu tıklayın
+        Thread.sleep(2000);
+        if(!checkBox2.isSelected()){
+            checkBox2.click();
+        }
+
+        driver.close();
+
+
+
+
+    }
+}
---
+++ src/test/java/RadioButton.java
@@ -0,0 +1,39 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class RadioButton {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","C:\\project\\chromedriver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.facebook.com");
+
+
+        //https://www.facebook.com adresine gidin
+        String url = "https://www.facebook.com";
+        driver.get(url);
+
+        //“Yeni Hesap Oluşturun” button’una basin
+        WebElement yeniHesapOlustur_Button = driver.findElement(By.xpath("//a[.='Yeni Hesap Oluştur']"));;
+        yeniHesapOlustur_Button.click();
+
+        //“radio buttons” elementlerini locate edin
+        Thread.sleep(2000);
+        WebElement erkek_RadioButton = driver.findElement(By.xpath("//input[@value=\"2\"]"));
+
+        //Secili degilse cinsiyet butonundan size uygun olani secin
+        if(!erkek_RadioButton.isSelected()){
+            //Thread.sleep(2000);
+            erkek_RadioButton.click();
+            //Thread.sleep(2000);
+        }
+
+        driver.close();
+
+    }
+}
