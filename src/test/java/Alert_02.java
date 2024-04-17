d4335589c3e533fec141f60e39576bfae3debdbc
refik <rfkkrklc95@gmail.com>
2024-04-17 20:35:24 +0300

    .

---
+++ src/test/java/Alert_02.java
@@ -0,0 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Alert_02 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver","C:\\project\\chromedriver\\chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts ");
+
+        WebElement alertButton2 = driver.findElement(By.xpath("//button[@onclick=\"jsConfirm()\"]"));
+        WebElement result = driver.findElement(By.id("result"));
+
+        alertButton2.click();
+        Thread.sleep(2000);
+        driver.switchTo().alert().dismiss();
+        // JS Alert isleminde Cancel tusuna basmak istiyorsak dismiss() methodunu kullanmamız gerekiyor
+        Thread.sleep(2000);
+
+        String expectedResult = "You clicked: Cancel";
+        String actualResult = result.getText();
+
+        if(actualResult.equals(expectedResult)){
+            System.out.println("Test 2 Passed");
+        }
+
+        Thread.sleep(2000);
+        driver.close();
+
+
+    }
+
+}
