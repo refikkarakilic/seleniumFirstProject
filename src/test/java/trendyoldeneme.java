ed76beba50967c99c62061f83ccc560ad59415e9
refik <rfkkrklc95@gmail.com>
2024-05-06 14:45:04 +0300

    .

--- src/test/java/trendyoldeneme.java
+++ src/test/java/trendyoldeneme.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -16,15 +17,25 @@ public class trendyoldeneme {
 
         driver.manage().window().maximize();
 
+        String url = "https://www.trendyol.com/";
+        driver.get(url);
 
+        //çerezleri reddet
+        driver.findElement(By.id("rejectAllButton")).click();
+
+        WebElement girisYapElement = driver.findElement(By.xpath("//p[.=\"Giriş Yap\"]"));
+
+        actions.moveToElement(girisYapElement).perform();
+        Thread.sleep(2000);
+
+        // login ekranına git
+        driver.findElement(By.xpath("(//div[.=\"Giriş Yap\"])[2]")).click();
+
+        //login ol
 
         //e-postayı gir ve şifreyi gir
         //e-posta : qaseleniummm@gmail.com
         //şifre : X123456..
-
-
-        //hepsiburada sitesine git
-        driver.get("https://www.trendyol.com/giris");
 
         WebElement mail = driver.findElement(By.id("login-email"));
         WebElement sifre = driver.findElement(By.name("login-password"));
@@ -37,6 +48,27 @@ public class trendyoldeneme {
         Thread.sleep(2000);
         loginBtn.click();
         Thread.sleep(2000);
+
+        // arama kısmına iphone 15 yaz ve arat
+
+        WebElement search = driver.findElement(By.xpath("(//input[@type=\"text\"])[1]"));
+
+        search.sendKeys("iphone 15", Keys.ENTER);
+
+        Thread.sleep(2000);
+
+        driver.close();
+
+
+
+
+
+
+
+
+
+
+
 
 
 
